3e2edfb0d14302c030981bcc05de3636e47886ba
JoelUnicda2 <joel.jimenez@unicda.edu.do>
2025-03-15 17:13:17 -0400

    Codelab Lesson 7 Use interface delegation

--- src/Example/AquariumFish.kt
+++ src/Example/AquariumFish.kt
@@ -5,21 +5,37 @@ abstract class AquariumFish   {
     abstract val color: String
 
 }
-class Shark: AquariumFish(), FishAction {
+
+class Shark: FishAction, FishColor {
     override val color = "grey"
     override fun eat() {
         println("hunt and eat fish")
     }
 }
 
-class Plecostomus: AquariumFish(), FishAction {
-    override val color = "gold"
-    override fun eat() {
-        println("eat algae")
-    }
-}
+
+class Plecostomus (fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor
+
 
 
 interface FishAction  {
     fun eat()
 }
+
+
+interface FishColor {
+    val color: String
+}
+
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
+
