7a243baf791e53fb861561eea5f3d8727e537e99
JoelUnicda2 <joel.jimenez@unicda.edu.do>
2025-03-15 16:54:01 -0400

    Codelab Lesson 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,25 @@
+/*fun buildAquarium() {
+    val aquarium1 = Aquarium()
+    aquarium1.printSize()
 
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+
+    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    aquarium4.printSize()
+} */
 
 
 fun buildAquarium() {
-   // val myAquarium = Aquarium()
-   // myAquarium.printSize()
-
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    myAquarium.height = 60
-    myAquarium.printSize()
-
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    aquarium6.volume = 70
+    aquarium6.printSize()
 }
+
 
 
 
